7cd5fc8b2798451d2a29c8500067989906127e31
Davis Birsan <davis.birsan@gmail.com>
2019-08-20 18:58:53 -0500

    updated small features

--- assets/javascript/action.js
+++ assets/javascript/action.js
@@ -17,13 +17,13 @@ $(".toggle").on("click", function () {
     }
 })
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.modal').modal();
-  });
+});
 
 $(function() {
 
-    $('a').each(function() { /* [1] */
+    $('.transition').each(function() { /* [1] */
          if ( location.hostname === this.hostname || !this.hostname.length ) { /* [1] */
 
             var link = $(this).attr("href"); /* [2] */
@@ -56,28 +56,28 @@ $(function() {
 
         }
     });
-  
+
 });
 
 $('.phone').hover(
     function () {
-      $('#hoverMe').show();
-    }, 
+        $('#hoverMe').show();
+    },
     function () {
-      $('#hoverMe').hide();
+        $('#hoverMe').hide();
     }
-  );
+);
 
-  $('.email').hover(
+$('.email').hover(
     function () {
-      $('#hoverMeTwo').show();
-    }, 
+        $('#hoverMeTwo').show();
+    },
     function () {
-      $('#hoverMeTwo').hide();
+        $('#hoverMeTwo').hide();
     }
-  );
+);
 
-  var hasBeenClickedPhone = false;
+var hasBeenClickedPhone = false;
 $(".phone-mobile").on("click", function () {
     if (hasBeenClickedPhone === false) {
         $('#hoverMe').show();
@@ -100,3 +100,59 @@ $(".email-mobile").on("click", function () {
         hasBeenClickedEmail = false;
     }
 })
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+  
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+  };
